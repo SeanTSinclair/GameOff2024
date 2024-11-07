6dc8330a82d8b6af4046f05a94939ff1563a8c7d
marius.bakkeli <marius.bakkeli@indykite.com>
2024-11-07 20:08:20 +0100

    feat: add sfx and sound bus

--- scripts/ui/options_menu.gd
+++ scripts/ui/options_menu.gd
@@ -3,17 +3,17 @@ extends CanvasLayer
 
 signal back_pressed
 
-@onready var sfx_slider: HSlider = %SfxSlider
-@onready var music_slider: HSlider = %MusicSlider
 @onready var back_button: Button = %BackButton
 @onready var display_button: Button = %DisplayButton
+@onready var sfx_slider: HSlider = %SfxSlider
+@onready var music_slider: HSlider = %MusicSlider
 
 
 func _ready() -> void:
 	back_button.pressed.connect(_on_back_button_pressed)
 	display_button.pressed.connect(_on_display_button_pressed)
-	#sfx_slider.value_changed.connect(_on_audio_slide_changed.bind("sfx"))
-	#music_slider.value_changed.connect(_on_audio_slide_changed.bind("music"))
+	sfx_slider.value_changed.connect(_on_audio_slide_changed.bind("sfx"))
+	music_slider.value_changed.connect(_on_audio_slide_changed.bind("music"))
 	_update_display()
 
 
@@ -21,8 +21,8 @@ func _update_display() -> void:
 	display_button.text = "Windowed"
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
 		display_button.text = "Fullscreen"
-	#sfx_slider.value = _get_bus_volume_precent("sfx")
-	#music_slider.value = _get_bus_volume_precent("music")
+	sfx_slider.value = _get_bus_volume_precent("sfx")
+	music_slider.value = _get_bus_volume_precent("music")
 
 
 func _on_display_button_pressed() -> void:
@@ -38,15 +38,18 @@ func _on_display_button_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	back_pressed.emit()
 
-#func _get_bus_volume_precent(bus_name: String) -> float:
-#var bus_index := AudioServer.get_bus_index(bus_name)
-#var volume_db := AudioServer.get_bus_volume_db(bus_index)
-#return db_to_linear(volume_db)
 
-#func _set_bus_volume_precent(bus_name: String, percent: float) -> void:
-#var bus_index := AudioServer.get_bus_index(bus_name)
-#var volume_db := linear_to_db(percent)
-#AudioServer.set_bus_volume_db(bus_index, volume_db)
+func _get_bus_volume_precent(bus_name: String) -> float:
+	var bus_index := AudioServer.get_bus_index(bus_name)
+	var volume_db := AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
 
-#func _on_audio_slide_changed(value: float, bus_name: String) -> void:
-#_set_bus_volume_precent(bus_name, value)
+
+func _set_bus_volume_precent(bus_name: String, percent: float) -> void:
+	var bus_index := AudioServer.get_bus_index(bus_name)
+	var volume_db := linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func _on_audio_slide_changed(value: float, bus_name: String) -> void:
+	_set_bus_volume_precent(bus_name, value)
