c9cb7a3f077ba843630cc0cbc65fb87586400bd0
Kristian Hiim <kristian.hiim@gmail.com>
2024-11-08 21:13:39 +0100

    refactor: use dictionary instead of array for inventory (#43)

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -1,10 +1,13 @@
 class_name Inventory
 extends Node
 
+signal item_added(item: Item)
+signal item_removed(item: Item)
+
 const ITEM_CARD = preload("res://scenes/inventory/item_card.tscn")
 
 var is_open: bool
-var items := Array()
+var items := Dictionary()
 
 @onready var inventory_menu: CanvasLayer = %InventoryMenu
 
@@ -27,17 +30,30 @@ func close() -> void:
 
 
 func add(item: Item) -> void:
-	items.push_back(item)
+	items[item.id] = item
 	update()
+	item_added.emit(item)
+
+
+func has(id: String) -> bool:
+	return items.has(id)
+
+
+func take(id: String) -> Item:
+	var item: Item = items.get(id)
+	if item:
+		items.erase(id)
+		item_removed.emit(item)
+	return item
 
 
 func update() -> void:
 	for child in items_flow_container.get_children():
 		child.queue_free()
 
-	for item in items:
+	for id in items.keys():
 		var box = ITEM_CARD.instantiate()
-		box.item = item
+		box.item = items.get(id)
 		box.item_clicked.connect(_on_item_clicked)
 		items_flow_container.add_child(box)
 
