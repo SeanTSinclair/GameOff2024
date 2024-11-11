35751016439827b195aaac711ce9dd4f961dce24
marius.bakkeli <marius.bakkeli@indykite.com>
2024-11-11 19:06:14 +0100

    refactor: add interaction manager to outline component

--- scripts/components/outline_component/outline_component.gd
+++ scripts/components/outline_component/outline_component.gd
@@ -2,10 +2,17 @@ class_name OutlineComponent
 extends Node
 
 @export var mesh_instance: MeshInstance3D
+@export var interaction_component: InteractionComponent
 
 var outline_material = preload(
 	"res://scenes/components/outline_component/outline_shader_material.tres"
 )
+
+
+func _ready() -> void:
+	if interaction_component:
+		interaction_component.entered.connect(show_outline)
+		interaction_component.exited.connect(hide_outline)
 
 
 func show_outline() -> void:
--- scripts/game_objects/box/box.gd
+++ scripts/game_objects/box/box.gd
@@ -4,15 +4,12 @@ extends RigidBody3D
 var blue_material = preload("res://scenes/game_objects/box/blue_material.tres")
 var red_material = preload("res://scenes/game_objects/box/red_material.tres")
 
-@onready var outline_component: OutlineComponent = $OutlineComponent
 @onready var interaction_component: InteractionComponent = $InteractionComponent
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
 
 func _ready() -> void:
 	interaction_component.interacted.connect(_on_interacted)
-	interaction_component.entered.connect(_on_entered)
-	interaction_component.exited.connect(_on_exited)
 
 
 func _on_interacted() -> void:
@@ -20,11 +17,3 @@ func _on_interacted() -> void:
 		mesh_instance.material_override = blue_material
 	else:
 		mesh_instance.material_override = red_material
-
-
-func _on_entered() -> void:
-	outline_component.show_outline()
-
-
-func _on_exited() -> void:
-	outline_component.hide_outline()
