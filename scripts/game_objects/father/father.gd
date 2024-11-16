326e4c9cdf6e9aeb4439258bafc6049156358cbe
bakkelim <mbakkeli@gmail.com>
2024-11-16 20:18:27 +0100

    feat: add interaction manager to father

--- scripts/game_objects/father/father.gd
+++ scripts/game_objects/father/father.gd
@@ -2,8 +2,14 @@ class_name Father
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $officialfather/AnimationPlayer
+@onready var interaction_component: InteractionComponent = $InteractionComponent
 
 
 func _ready():
 	animation_player.play("Idle")
 	animation_player.get_animation("Idle").loop_mode = Animation.LOOP_LINEAR
+	interaction_component.interacted.connect(_on_interacted)
+
+
+func _on_interacted() -> void:
+	print("implement talk to father here")
