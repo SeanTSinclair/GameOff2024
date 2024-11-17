10e26ed007421287318419ee70581d3f7338824b
marius.bakkeli <marius.bakkeli@indykite.com>
2024-11-17 15:21:34 +0100

    feat: add grandmother

---
+++ scripts/game_objects/grandmother/grandmother.gd
@@ -0,0 +1,15 @@
+class_name Grandmother
+extends Node3D
+
+@onready var interaction_component: InteractionComponent = $InteractionComponent
+@onready var animation_player: AnimationPlayer = $GrannOfficialAnimation/AnimationPlayer
+
+
+func _ready():
+	animation_player.play("Sitting_Idle")
+	animation_player.get_animation("Sitting_Idle").loop_mode = Animation.LOOP_LINEAR
+	interaction_component.interacted.connect(_on_interacted)
+
+
+func _on_interacted() -> void:
+	print("implement talk to grandmother here")
