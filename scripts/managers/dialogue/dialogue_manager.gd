bda0debf2cf91ba10d07ba968d9e6bd08153274c
Aina Kvamme Bru <ainakvammebru@gmail.com>
2024-11-27 19:56:48 +0100

    added back dia manager

---
+++ scripts/managers/dialogue/dialogue_manager.gd
@@ -0,0 +1,57 @@
+extends Node
+
+var player
+var npcs = []
+var current_npc: Node = null
+
+
+# Set the player reference
+func set_player(player_ref):
+	player = player_ref
+
+
+# Set the NPC references
+func set_npcs(npc_list):
+	npcs = npc_list
+
+
+func register_npc(npc):
+	if not npcs.has(npc):
+		npcs.append(npc)
+		npc.connect("interacted", Callable(self, "start_dialogue"))
+
+
+func unregister_npc(npc):
+	if npcs.has(npc):
+		npcs.erase(npc)
+
+
+#test
+func start_dialogue(npc_reference: Node = null):
+	current_npc = npc_reference
+	var timeline_name = current_npc.timeline
+	if player == null:
+		print("Error: Player not set!")
+		return
+	freeze_all()
+	Dialogic.start(timeline_name)
+	Dialogic.timeline_ended.connect(_on_dialogue_end)
+
+
+func _on_dialogue_end():
+	Dialogic.timeline_ended.disconnect(_on_dialogue_end)
+	unfreeze_all()
+
+
+func freeze_all():
+	if player:
+		player.freeze()
+	if current_npc:
+		current_npc.freeze()
+
+
+func unfreeze_all():
+	if player:
+		player.unfreeze()
+	if current_npc:
+		current_npc.unfreeze()
