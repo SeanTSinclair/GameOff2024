d89945a2f22184df2dad7f0a3a6875483bcafacc
Marius Tendeland Horne <marius_t_h@hotmail.com>
2024-11-27 22:36:10 +0100

    Feat: add key that opens doors

--- scripts/autoloaders/state.gd
+++ scripts/autoloaders/state.gd
@@ -6,6 +6,8 @@ var initial_power = 0.0
 var power = 0.0
 var initial_secrets_found = {}
 var secret_texts_found = {}
+var initial_keys_found = {}
+var keys_found = {}
 
 
 func reset() -> void:
@@ -35,3 +37,11 @@ func add_secret_found(secret_text_id: String):
 
 func is_secret_found(secret_text_id: String):
 	return secret_texts_found.get(secret_text_id)
+
+
+func add_key_found(key_id: String):
+	keys_found[key_id] = true
+
+
+func is_key_found(key_id: String):
+	return keys_found.get(key_id) != null
