7eb0e1db1e94eb58a0e47b8473efc06bf151e9cd
ostenespen <espen.susort@gmail.com>
2024-11-29 15:36:52 +0100

    added stuff
    added stuff

---
+++ scenes/omni_light_3d.gd
@@ -0,0 +1,19 @@
+extends OmniLight3D
+
+@export var min_energy: float = 1  # Minimum brightness
+@export var max_energy: float = 3  # Maximum brightness
+@export var flicker_interval: float = 0.1  # Time between flickers
+
+func _ready():
+	var timer: Timer = $FlickerTimer  # Replace with your Timer node's name or path if different
+	if timer:
+		print("Timer found")
+		# Connect the signal using Callable()
+		timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
+		timer.start(flicker_interval)
+	else:
+		print("Timer not found")
+
+func _on_Timer_timeout():
+	# Change the light's energy using the correct property
+	light_energy = randf_range(min_energy, max_energy)
