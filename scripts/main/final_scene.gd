30567a13270a6320aa380fc45fe67ca01b3c243c
Sean Sinclair <50667835+SeanTSinclair@users.noreply.github.com>
2024-12-01 00:21:05 +0100

    Final scene + background music loop

---
+++ scripts/main/final_scene.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var player: Player = $Player
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	player.can_move = false
+	animation_player.play("final_scene")
+
+
+func queue_hover() -> void:
+	animation_player.play("hover")
