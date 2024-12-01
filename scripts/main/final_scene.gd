aec5f50f2f980c5dd628de46e0f26d1a30be228b
Sean Sinclair <50667835+SeanTSinclair@users.noreply.github.com>
2024-12-01 14:36:18 +0100

    Made dialogic event start fire animation (#206)

--- scripts/main/final_scene.gd
+++ scripts/main/final_scene.gd
@@ -7,6 +7,7 @@ extends Node3D
 func _ready() -> void:
 	player.can_move = false
 	animation_player.play("final_scene")
+	Dialogic.signal_event.connect(_burn_scene_run)
 
 
 func queue_hover() -> void:
@@ -16,3 +17,8 @@ func queue_hover() -> void:
 
 func show_being_moved_by_force_text() -> void:
 	Events.feedback.emit("What? I'm being moved against my will!")
+
+
+func _burn_scene_run(arg) -> void:
+	if arg == "burn_baby_burn":
+		animation_player.play("burn_baby_burn")
