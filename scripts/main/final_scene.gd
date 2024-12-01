13d655ce125684e1ce88f11d4daf62a66f0e06b2
Aina <93499402+AinaKvammeBru@users.noreply.github.com>
2024-12-01 15:26:39 +0100

    cleaned up code and final scene playing logic (#210)

--- scripts/main/final_scene.gd
+++ scripts/main/final_scene.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const CREDITS = preload("res://scenes/ui/credits/credits.tscn")
+
 @onready var player: Player = $Player
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -7,7 +9,8 @@ extends Node3D
 func _ready() -> void:
 	player.can_move = false
 	animation_player.play("final_scene")
-	Dialogic.signal_event.connect(_burn_scene_run)
+	Dialogic.signal_event.connect(_final_scene_run)
+	animation_player.animation_finished.connect(_animation_finished)
 
 
 func queue_hover() -> void:
@@ -19,6 +22,16 @@ func show_being_moved_by_force_text() -> void:
 	Events.feedback.emit("What? I'm being moved against my will!")
 
 
-func _burn_scene_run(arg) -> void:
+func _final_scene_run(arg) -> void:
 	if arg == "burn_baby_burn":
 		animation_player.play("burn_baby_burn")
+	elif arg == "family_end":
+		get_tree().change_scene_to_packed(CREDITS)
+
+
+func _animation_finished(animation_name: String):
+	print("animation finished")
+	if animation_name == "burn_baby_burn":
+		get_tree().change_scene_to_packed(CREDITS)
+	#elif animation_name == "family_end":
+	#get_tree().change_scene_to_packed(CREDITS)
