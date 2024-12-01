95a9e1566b46f704d731bba369e6f114d441777a
Kristian Hiim <870647+khiim@users.noreply.github.com>
2024-12-01 18:39:01 +0100

    bad ending animation

--- scripts/main/final_scene.gd
+++ scripts/main/final_scene.gd
@@ -30,14 +30,16 @@ func _final_scene_run(arg) -> void:
 		SoundManager.play_sfx("flames")
 	elif arg == "family_end":
 		SoundManager.play_bgm("BGM")
-		get_tree().change_scene_to_packed(CREDITS)
+		animation_player.play("die")
+		SoundManager.play_sfx("dying")
 
 
 func _animation_finished(animation_name: String):
 	print("animation finished")
 	if animation_name == "burn_baby_burn":
 		SoundManager.stop("flames")
+		SoundManager.stop("dying")
 		SoundManager.play_bgm("BGM")
 		get_tree().change_scene_to_packed(CREDITS)
-	#elif animation_name == "family_end":
-	#get_tree().change_scene_to_packed(CREDITS)
+	elif animation_name == "die":
+		get_tree().change_scene_to_packed(CREDITS)
