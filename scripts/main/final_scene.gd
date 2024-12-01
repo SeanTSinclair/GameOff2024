96c298e7958de341889b6c00a2373df9f7a9036d
Aina Kvamme Bru <ainakvammebru@gmail.com>
2024-12-01 18:12:59 +0100

    sounds for final_scene.gd

--- scripts/main/final_scene.gd
+++ scripts/main/final_scene.gd
@@ -9,6 +9,8 @@ const CREDITS = preload("res://scenes/ui/credits/credits.tscn")
 func _ready() -> void:
 	player.can_move = false
 	animation_player.play("final_scene")
+	SoundManager.stop("BGM")
+	SoundManager.play_sfx("hoover_animation")
 	Dialogic.signal_event.connect(_final_scene_run)
 	animation_player.animation_finished.connect(_animation_finished)
 
@@ -25,13 +27,17 @@ func show_being_moved_by_force_text() -> void:
 func _final_scene_run(arg) -> void:
 	if arg == "burn_baby_burn":
 		animation_player.play("burn_baby_burn")
+		SoundManager.play_sfx("flames")
 	elif arg == "family_end":
+		SoundManager.play_bgm("BGM")
 		get_tree().change_scene_to_packed(CREDITS)
 
 
 func _animation_finished(animation_name: String):
 	print("animation finished")
 	if animation_name == "burn_baby_burn":
+		SoundManager.stop("flames")
+		SoundManager.play_bgm("BGM")
 		get_tree().change_scene_to_packed(CREDITS)
 	#elif animation_name == "family_end":
 	#get_tree().change_scene_to_packed(CREDITS)
